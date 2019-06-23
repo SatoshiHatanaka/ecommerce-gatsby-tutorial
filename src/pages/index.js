9adcd8113a3b2b6e312a5052b45e474696a44d16
Satoshi Hatanaka <developer.satoshi.hatanaka@gmail.com>
2019-06-23 14:33:09 +0900

    first commit

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,51 @@
 import React from "react"
-import { Link } from "gatsby"
+// import { Link } from "gatsby"
 
 import Layout from "../components/layout"
-import Image from "../components/image"
+// import Image from "../components/image"
 import SEO from "../components/seo"
+
+class Button extends React.Component {
+  componentDidMount() {
+    this.stripe = window.Stripe("pk_test_Ye6VhjEMNwhlSfKQ8N1FdRJx00Emb9QMsj", {
+      betas: ["checkout_beta_4"],
+    })
+  }
+  render() {
+    return (
+      <form onSubmit={event => {
+        event.preventDefault()
+        this.stripe.redirectToCheckout({
+          items: [{sku: 'sku_FIJLVAqEnM5MQd', quantity: 1}],
+    
+          // Do not rely on the redirect to the successUrl for fulfilling
+          // purchases, customers may not always reach the success_url after
+          // a successful payment.
+          // Instead use one of the strategies described in
+          // https://stripe.com/docs/payments/checkout/fulfillment
+          successUrl: 'http://localhost:8000/success',
+          cancelUrl: 'http://localhost:8000/canceled',
+        })
+        .then(function (result) {
+          if (result.error) {
+            // If `redirectToCheckout` fails due to a browser or network
+            // error, display the localized error message to your customer.
+            var displayError = document.getElementById('error-message');
+            displayError.textContent = result.error.message;
+          }
+        });
+      }}>
+        <button type="submit">Buy</button>
+      </form>
+    )
+  }
+}
 
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
-    <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
-    </div>
-    <Link to="/page-2/">Go to page 2</Link>
+    <h1>Buy my products!</h1>
+    <Button />
   </Layout>
 )
 
